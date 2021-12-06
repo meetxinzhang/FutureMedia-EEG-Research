6f308aad18ce759743b60744c5f01ca41d9562cd
Xin Zhang <meetdevin.zh@outlook.com>
2021-12-06 13:44:47 +0800

    20211206  find events from stimulus channel

--- data_pipeline/bdf_reader.py
+++ data_pipeline/bdf_reader.py
@@ -12,11 +12,11 @@ class BDFReader(object):
     def __init__(self, file_path='/media/xin/Raid0/dataset/CVPR2021-02785/data/imagenet40-1000-1-00.bdf'):
         self.selection = []
         self.file_path = file_path
-        self.EEG_datas, self.EEG_times = self.read()
+        self.EEG_datas, self.EEG_times = self.read_as_events()
 
-    def read(self):
+    def read_as_paper(self):
+        # divide bdf into 400 samples according to the describing in paper
         bdf = mne.io.read_raw_bdf(self.file_path, preload=True)
-
         picks = mne.pick_types(bdf.info, eeg=True, stim=False,
                                exclude=['EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8', 'Status'])
         EEG_datas = []
@@ -31,12 +31,41 @@ class BDFReader(object):
             EEG_times.append(times[0])
         return EEG_datas, EEG_times
 
+    def read_as_events(self):
+        # see https://mne.tools/dev/generated/mne.find_events.html#mne.find_events for more details
+        bdf = mne.io.read_raw_bdf(self.file_path, preload=True)
+        picks = mne.pick_types(bdf.info, eeg=True, stim=False,
+                               exclude=['EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8', 'Status'])
+
+        events = mne.find_events(bdf, stim_channel='Status', initial_event=True, output='step')
+
+        start_time = []
+        for event in events:
+            if event[1] == 65280 and event[2] == 65281:
+                start_time.append(event[0])
+            if event[1] == 65280 and event[2] == 0:
+                start_time.append(event[0])  # the last sample is contact with 10s blocking
+
+        EEG_datas = []
+        EEG_times = []
+        for i in range(len(start_time) - 1):
+            start = start_time[i]
+            # if i == 398:
+            end = start + 8192  # each sample lasting 2s, 2*1000*4.096=8192
+            # else:
+            #     end = start_time[i + 1]
+
+            data, times = bdf[picks, start:end]
+            EEG_datas.append(data.T)
+            EEG_times.append(times[0])
+        return EEG_datas, EEG_times
+
     def get_item_matrix(self, file_path, sample_idx):
         if file_path == self.file_path:
             return self.EEG_datas[sample_idx]
         else:
             self.file_path = file_path
-            self.EEG_datas, self.EEG_times = self.read()
+            self.EEG_datas, self.EEG_times = self.read_as_events()
             return self.EEG_datas[sample_idx]
 
 
--- model/__init__.py
+++ model/__init__.py
@@ -6,34 +6,78 @@
 @desc:
 """
 import mne
+import numpy as np
 
-bdf = mne.io.read_raw_bdf('/media/xin/Raid0/dataset/CVPR2021-02785/data/imagenet40-1000-1-00.bdf', preload=True)
+bdf = mne.io.read_raw_bdf('/media/xin/Raid0/dataset/CVPR2021-02785/data/imagenet40-1000-1-09.bdf', preload=True)
+bdf.plot(start=10, duration=1)
 
 print(bdf.info, '\n')
 print('channels: ', len(bdf.ch_names), bdf.ch_names, '\n')
 print('times: ', bdf.n_times, bdf.times, '\n')
 
-events = mne.find_events(bdf, initial_event=True)
-events[:, 2] &= (2**16 - 1)
+events1 = mne.find_events(bdf, stim_channel='Status', initial_event=True, output='step')
+# events2 = mne.find_events(bdf, stim_channel='Status', initial_event=True, output='onset')
+# events3 = mne.find_events(bdf, stim_channel='Status', initial_event=True, output='offset')
+# events[:, 2] &= (2**16 - 1)
 
-print(events)
+# np.set_printoptions(threshold=np.inf)
+print(np.shape(events1), '\n', events1)
+# print(np.shape(events2), '\n', events2)
+# print(np.shape(events3), '\n', events3)
+#
+# start_time = []
+# for event in events1:
+#     if event[1] == 65280 and event[2] == 65281:
+#         start_time.append(event[0])
+#
+# print('\nlength: ', len(start_time))
 
+# bdf.pick_types(eeg=False, stim=True).plot()
 """
-402 events found
-Event IDs: [ 65281 131069 131071]
-[[      0       0   65535]
- [    977  130816   65533]
- [  43737   65280   65281]
+403 events found
+Event IDs: [     0  65281 130816 131069]
+(403, 3) 
+ [[      0       0  130816]
+ [   1919  130816  131069]
+ [  43586   65280   65281]
  ...
- [4949078   65280   65281]
- [4961435   65280   65281]
- [4973791   65280   65281]]
+ [4961294   65280   65281]
+ [4973650   65280   65281]
+ [5021696   65280       0]]
  
- see https://mne.tools/dev/generated/mne.find_events.html#mne.find_events for details
+length: 400
+ 
+ see https://mne.tools/dev/generated/mne.find_events.html#mne.find_events for more details
 """
+picks = mne.pick_types(bdf.info, eeg=True, stim=False,
+                       exclude=['EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8', 'Status'])
+start_time = []
+for event in events1:
+    if event[1] == 65280 and event[2] == 65281:
+        start_time.append(event[0])
+    if event[1] == 65280 and event[2] == 0:
+        start_time.append(event[0])  # the last sample is contact with 10s blocking
 
+print('\nlength: ', start_time)
 
-# df = bdf.to_data_frame(index='time', time_format=None)
+EEG_datas = []
+EEG_times = []
+for i in range(len(start_time)-1):
+    start = start_time[i]
+    # if i == 398:
+    end = start + 8192
+    # else:
+    #     end = start_time[i + 1]
+    print(start, end)
+
+    data, times = bdf[picks, start:end]
+    EEG_datas.append(data.T)
+    EEG_times.append(times[0])
+    print(times)
+print('aaaaaaaaa', len(EEG_datas))
+# print(EEG_times)
+
+# df = bdf.to_data_frame(index='time', time_format='ms')
 # print(df)
 
 # Status = df['Status']
