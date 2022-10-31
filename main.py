2752a280cb8f02d8ed6270b55918fe078158733f
Xin Zhang <MeetDevin.zh@outlook.com>
2022-10-31 09:43:58 +0800

    control experiments

--- main.py
+++ main.py
@@ -16,24 +16,25 @@ from model.lrp_manager import ignite_relprop, generate_visualization
 
 summary = SummaryWriter(log_dir='./log/')
 gpu = torch.cuda.is_available()
-# torch.cuda.set_device(1)
+torch.cuda.set_device(6)
 batch_size = 32
-n_epoch = 100
+n_epoch = 1000
 total_x = 323  # 400 * 100
 
 # ../../Datasets/run16/pkl
 # E:/Datasets/CVPR2021-02785/pkl
-dataset = SZUDataset(path='E:/Datasets/eegtest/run16/pkl')
-loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_, num_workers=4)
+# E:/Datasets/eegtest/run16/pkl
+dataset = SZUDataset(path='../../Datasets/run00')
+loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_, num_workers=4, shuffle=True)
 
 ff = FieldFlow(dim=40, num_heads=5, mlp_dilator=2, qkv_bias=False, drop_rate=0.2, attn_drop_rate=0.2,
-               t=500, n_signals=127, n_classes=40)
+               t=512, n_signals=96, n_classes=40)
 
 # ff.load_state_dict(torch.load('log/checkpoint/2022-10-28-17-26-04.pkl'))
 if gpu:
     ff.cuda()
 
-optimizer = torch.optim.Adam(ff.parameters(), lr=0.002, betas=(0.9, 0.98), eps=1e-9)
+optimizer = torch.optim.Adam(ff.parameters(), lr=0.0003, betas=(0.9, 0.98), eps=1e-9)
 # optimizer = NoamOpt(model_size=40, factor=1, warmup=8000,
 #                     optimizer=torch.optim.Adam(ff.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
 
@@ -78,22 +79,22 @@ if __name__ == '__main__':
 
             step += 1
             global_step += 1
-            if step % 10 == 0:
+            if step % 1 == 0:
                 corrects = (torch.argmax(y, dim=1).data == label.data)
                 accuracy = corrects.cpu().int().sum().numpy() / batch_size
-                print('epoch:{}/10 step:{}/{} global_step:{} '
-                      'loss={:.5f} acc={:.3f} lr={}'.format(epoch, step, int(total_x / batch_size), global_step,
+                print('epoch:{}/{} step:{}/{} global_step:{} '
+                      'loss={:.5f} acc={:.3f} lr={}'.format(epoch, n_epoch, step, int(total_x / batch_size), global_step,
                                                             loss, accuracy, lr))
                 summary.add_scalar(tag='TrainLoss', scalar_value=loss, global_step=global_step)
                 summary.add_scalar(tag='TrainAcc', scalar_value=accuracy, global_step=global_step)
 
-            if step % 50 == 0:
+            if step % 5 == 0:
                 cam = ignite_relprop(model=ff, x=x[0].unsqueeze(0), index=label[0])  # [1, 1, 512, 96]
                 generate_visualization(x[0].squeeze(), cam.squeeze(),
                                        save_name='S' + str(global_step) + '_C' + str(label[0].cpu().numpy()))
 
         step = 0
     current_info = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-    torch.save(ff.state_dict(), 'log/checkpoint/'+current_info+'.pkl')
+    torch.save(ff.state_dict(), 'log/checkpoint/'+current_info+'_1000e.pkl')
     summary.close()
     print('done')
