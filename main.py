ee28f0be377175be547d55b077c3b514b1085ac5
Xin Zhang <MeetDevin.zh@outlook.com>
2022-11-23 22:07:00 +0800

    EEGNet

--- main.py
+++ main.py
@@ -11,31 +11,33 @@ import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 import time
 from data_pipeline.dataset_szu import SZUDataset, collate_
-from model.field_flow import FieldFlow
-from model.lrp_manager import ignite_relprop, generate_visualization
-from utils.weight_init import get_state_dict
+from model.eeg_net import EEGNet
+# from model.field_flow import FieldFlow
+# from model.lrp_manager import ignite_relprop, generate_visualization
+# from utils.weight_init import get_state_dict
 
 # torch.cuda.set_device(6)
 batch_size = 32
 n_epoch = 2000
 
-id_experiment = '_2000e1l-set1-4-96dim'
+id_experiment = '_2000e03l-set1-4-EEGNet'
 t_experiment = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
 
-# ../../Datasets/run00
-# ../../Datasets/run16/pkl_ave
+# ../../Datasets/pkl_ave
 # E:/Datasets/CVPR2021-02785/pkl
-# E:/Datasets/eegtest/run16/pkl_ave
+# E:/Datasets/SZFace2/EEG/pkl_ave
 dataset = SZUDataset(path='../../Datasets/pkl_ave', contains='subject1', endswith='.pkl')
 total_x = dataset.__len__()
-print(total_x, ' !!!!!!')
+print(total_x, ' link start !!!!!!')
 loader = torch.utils.data.DataLoader(dataset, collate_fn=collate_, batch_size=batch_size, num_workers=4,
-                                     prefetch_factor=2, shuffle=True)
+                                     prefetch_factor=2, shuffle=True, drop_last=True)
 
-ff = FieldFlow(dim=96, num_heads=6, mlp_dilator=2, qkv_bias=False, drop_rate=0.2, attn_drop_rate=0.2,
-               t=500, n_signals=127, n_classes=40).cuda()
+# ff = FieldFlow(dim=96, num_heads=6, mlp_dilator=2, qkv_bias=False, drop_rate=0.2, attn_drop_rate=0.2,
+#                t=500, n_signals=127, n_classes=40).cuda()
 # ff.load_state_dict(get_state_dict('log/checkpoint/2022-11-04-15-59-42_1000e03l-pre.pkl',
 #                                   map_location='cuda:0', exclude=['arc_margin.weight']))
+ff = EEGNet(classes_num=40, drop_out=0.25).cuda()
+
 optimizer = torch.optim.AdamW(ff.parameters(), lr=0.0003, betas=(0.9, 0.98), eps=1e-9)
 # optimizer = NoamOpt(model_size=40, factor=1, warmup=8000,
 #                     optimizer=torch.optim.Adam(ff.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
@@ -59,7 +61,7 @@ if __name__ == '__main__':
     global_step = 0
     for epoch in range(1, n_epoch + 1):
         for x, label in loader:
-            #  [b, 1, 512, 96], [b]
+            #  [b, 1, 500, 127], [b]
             if x is None and label is None:
                 step += 1
                 global_step += 1
@@ -71,7 +73,7 @@ if __name__ == '__main__':
             # if step % 2 == 0:
             optimizer.zero_grad()  # clean grad per 2 step, to double the batch_size
 
-            y = ff(x, label=None)  # [bs, 40]
+            y = ff(x)  # [bs, 40]
             loss = F.cross_entropy(y, label)
             loss.backward()
             optimizer.step()
